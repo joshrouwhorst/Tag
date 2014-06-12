c38072d4d52a62eff34446982db78d5e25851d82
unknown <achau@st-l-10276.springthrough.local>
2014-06-12 16:33:56 -0400

    setup sockets

--- client/js/socketListener.js
+++ client/js/socketListener.js
@@ -1,5 +1,17 @@
 var socket = io.connect();
 
-socket.on('serverResponse', function(data) {
-   alert('Server said: ' + data);
+//server asked for my name
+socket.on('whatsYoName', function() {
+   var name = window.prompt("What's your name?", "");
+   socket.emit('heresMyName', name);
+});
+
+//someone connected
+socket.on('welcome', function(player) {
+   alert(player.name + " has connected!");
+});
+
+//someone left
+socket.on('left', function(name) {
+   alert(name + " has left!");
 });
