a08acd35e54f56229bc486eced667e3488bfc62f
Josh Rouwhorst <jrouwhorst0@gmail.com>
2014-06-12 16:28:32 -0400

    Started player class

--- client/js/socketListener.js
+++ client/js/socketListener.js
@@ -3,3 +3,7 @@ var socket = io.connect();
 socket.on('serverResponse', function(data) {
    alert('Server said: ' + data);
 });
+
+socket.on('updatedPlayers', function(data){
+  Players.update(data);
+});
