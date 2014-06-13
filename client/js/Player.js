003c5b1ce9f03e38c25d5340947f2030c044b32d
Josh Rouwhorst <jrouwhorst0@gmail.com>
2014-06-13 10:05:44 -0400

    Removing move functionality from Player

--- client/js/Player.js
+++ client/js/Player.js
@@ -9,31 +9,51 @@ var Players = (function(){
   };
 
   var update = function(data){
+    var found;
+
     //Find out what the user's pushing
     if (!data) {
       return null;
     }
 
-    // Clears out the array but keeps all references to the array sync'd.
-    players.splice( 0, players.length );
+    for ( var i = 0; i < players.length; i++ ){
+      found = false;
 
-    for ( var i = 0; i < data.length; i++ ){
-      players.push( new Player( data[i] ) );
-      //Find logged in player and keep track of them.
+      for ( var j = 0; j < data.length && !found; j++ ){
+        if ( players[i].id === data[j].id ){
+          players[i].update( data[j] );
+          found = true;
+        }
+      }
 
-      if ( data[i].isUser ){
-        currentPlayer = players[ players.length - 1 ];
+      if ( !found ){
+        players.splice(i, 1);
+        i--;
       }
     }
   };
-
-  this.draw = noop;
 
   var getPlayers = function(){
     return players;
   };
 
+  var getCurrentPlayer = function(){
+    return currentPlayer;
+  };
+
+  var getPlayerById = function( id ){
+    for ( var i = 0; i < players.length; i++ ){
+      if ( players[i].id == id ){
+        return players[i];
+      }
+    }
+
+    return null;
+  };
+
   return {
+    getPlayerById: getPlayerById,
+    getCurrentPlayer: getCurrentPlayer,
     getPlayers: getPlayers,
     update: update,
     draw: noop,
@@ -48,47 +68,10 @@ var Player = function( properties ){
   this.init = noop;
   this.draw = noop;
 
-  this.update = function(){
-    var up = UserInput.is('up'),
-        left = UserInput.is('left'),
-        right = UserInput.is('right'),
-        down = UserInput.is('down'),
-        newX = that.x, newY = that.y;
-
-    moveThisMuch = level.TileSize * that.speed;
-
-    if ( up && left ){
-      moveThisMuch = moveThisMuch / 2;
-      newX = that.x + moveThisMuch;
-      newY = that.y - moveThisMuch;
-    } else if ( up && right ) {
-      moveThisMuch = moveThisMuch / 2;
-      newX = that.x - moveThisMuch;
-      newY = that.y - moveThisMuch;
-    } else if ( down && left ) {
-      moveThisMuch = moveThisMuch / 2;
-      newX = that.x + moveThisMuch;
-      newY = that.y + moveThisMuch;
-    } else if ( down && right ) {
-      moveThisMuch = moveThisMuch / 2;
-      newX = that.x - moveThisMuch;
-      newY = that.y + moveThisMuch;
-    } else if ( left ) {
-      newX = that.x + moveThisMuch;
-    } else if ( down ) {
-      newY = that.y + moveThisMuch;
-    } else if ( right ) {
-      newX = that.x - moveThisMuch;
-    } else if ( up ) {
-      newY = that.y - moveThisMuch;
+  this.update = function( _properties ){
+    for (var attr in _properties){
+      properties[attr] = _properties[attr];
     }
-
-    // TODO: Call level to check if there is anything at the new coords.
-
-    that.x = Math.round(newX);
-    that.y = Math.round(newY);
-
-    // TODO: Send new coords to the server.
   };
 
   this.isUser = function(){
@@ -119,6 +102,10 @@ var Player = function( properties ){
         isTagged: false,
         speed: 1
       };
+
+  if ( properties.id ) {
+    this.id = properties.id;
+  }
 
   for ( var attr in defaultValues ){
     if ( properties[attr] === undefined ){
