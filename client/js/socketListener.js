aecb21f8ef3ddefcee636a8205209fb19dc879ae
Alex Chau <chaua@mail.gvsu.edu>
2014-06-12 22:28:21 -0400

    cleaned up socket listeners

--- client/js/socketListener.js
+++ client/js/socketListener.js
@@ -6,22 +6,17 @@ socket.on('whatsYoName', function() {
 
 //someone connected
 socket.on('welcome', function(player) {
-   alert(player.name + " has connected!");
-   players.push(new Player(player.id, player.name));
+   console.log(player.name + " has connected!");
 });
 
 //someone left
 socket.on('left', function(name) {
-   alert(name + " has left!");
+   console.log(name + " has left!");
 });
 
-socket.on('serverResponse', function(data) {
-   alert('Server said: ' + data);
+socket.on('updatedPlayers', function(data){
+  players = data;
 });
-
-/*socket.on('updatedPlayers', function(data){
-  PlayerList.update(data);
-});*/
 
 socket.on("heresTheLevel", function(data){
   level.init(data);
